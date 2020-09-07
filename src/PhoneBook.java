af68422dea42f668dac6520a6a8ab9668074cb50
Fomina Aleksandra <sashavera1998@gmail.com>
2020-09-07 22:47:44 +0500

    isCorrectName, formatName

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class PhoneBook {
 
     public static void main(String[] args) {
         //Добавить считывание ввода пользователя в цикле
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String[][] book = new String[1][2];
+        while (!name.toLowerCase().equals("exit")){
+            if (!isCorrectName(name)) System.out.println("Введите корректное имя!");
+            else {
+                String formatName = formatName(name);
+                System.out.println(formatName);
+//                if(checkName(formatName)){
+//
+//                }
+            }
+            name = scanner.nextLine();
+        }
     }
 
     public static boolean checkPhoneNumber(String phoneNumber) {
@@ -11,9 +27,23 @@ public class PhoneBook {
     public static boolean checkName(String name) {
         return true;
     }
+    public static boolean isCorrectName(String name) {
+        String[] checked = name.trim().split(" ");
+        if (checked.length == 3) {
+            if (!(checked[0]+checked[1]+checked[2]).contains(" ")) return true;
+            else return false;
+        }
+        return false;
+    }
 
     public static String formatName(String name) {
-        return "";
+        String[] words = name.trim().split(" ");
+        String res = "";
+        for (int i = 0; i < words.length; ++i) {
+            words[i] = words[i].replaceFirst(("" + words[i].charAt(0)), ("" + words[i].charAt(0)).toUpperCase());
+            res= res.concat(words[i].concat(" "));
+        }
+        return res.trim();
     }
 
     public static String formatPhoneNumber(String number) {

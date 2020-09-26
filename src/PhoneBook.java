aa32320e315fa214ea868416c82d6b577db12221
Alexandra Piterova <alexandra.piterova@gmail.com>
2020-09-26 17:04:12 +0300

    Added main method (getting names)

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -3,16 +3,27 @@ import java.util.Scanner;
 public class PhoneBook {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        boolean isCorrectNumber = false;
 
+        boolean isCorrectName = false;
+        while (!isCorrectName) {
+            System.out.println("Введите ФИО:");
+            String name = scanner.nextLine();
+            isCorrectName = checkName(name);
+            if (!isCorrectName) {
+                System.out.println("Имя некорректное!");
+            } else {
+                System.out.println(formatName(name));
+            }
+        }
+
+        boolean isCorrectNumber = false;
         while (!isCorrectNumber) {
             System.out.println("Введите номер телефона:");
             String phoneNumber = scanner.nextLine();
             isCorrectNumber = checkPhoneNumber(phoneNumber);
             if (!isCorrectNumber) {
                 System.out.println("Номер некорректный!");
-            }
-            else {
+            } else {
                 System.out.println(formatPhoneNumber(phoneNumber));
             }
         }
@@ -40,8 +51,8 @@ public class PhoneBook {
 
     public static String formatPhoneNumber(String number) {
         String cleanNumber = number.replaceAll("[^0-9]", "");
-        return "8 " + cleanNumber.substring(1,4) + " " + cleanNumber.substring(4,7)
-                + " " + cleanNumber.substring(7,9) + " " + cleanNumber.substring(9);
+        return "8 " + cleanNumber.substring(1, 4) + " " + cleanNumber.substring(4, 7)
+                + " " + cleanNumber.substring(7, 9) + " " + cleanNumber.substring(9);
     }
 
     public static void add(String[][] book, String name, String number) {
